c19e23a290ca11c0766d6ca4da38aff6a602a17a
Jesus Guerra <jguerrix@gmail.com>
2018-12-31 15:19:51 -0500

    Update Home Screen UI

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Styles from "./style";
-import { View, Text, Button } from "react-native";
-import DeviceInfo from 'react-native-device-info';
+import { View, Text, Button, ActivityIndicator } from "react-native";
+import DeviceInfo from "react-native-device-info";
 
 export default class HomeScreen extends React.Component {
-  state={
-    deviceId: ''
-  }
+  state = {
+    isLoading: true,
+    deviceId: ""
+  };
   componentDidMount() {
-    this.getDeviceID()
+    this.getDeviceID();
   }
-  
+
   /**
    * getDeviceID
    *
@@ -18,33 +19,82 @@ export default class HomeScreen extends React.Component {
    */
   getDeviceID = () => {
     const deviceId = DeviceInfo.getUniqueID();
-    this.setState({
-      deviceId
-    }, () => this.registerUser())
-  }
-  
+    this.setState(
+      {
+        deviceId
+      },
+      () => this.registerUser()
+    );
+  };
+
   /**
    * registerUser
    *
    * Registers user on the server and returns user info
    */
   registerUser = () => {
-  
-  }
-  
+    this.setState({
+      isLoading: true
+    });
+    setTimeout(() => {
+      this.setState({
+        isLoading: false
+      });
+    }, 2500);
+  };
+
   static navigationOptions = {
     header: null
   };
 
   render() {
-    return (
-      <View style={Styles.mainContainer}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Start Game"
-          onPress={() => this.props.navigation.navigate("Game")}
-        />
-      </View>
-    );
+    let content;
+    if (this.state.isLoading) {
+      content = <ActivityIndicator size="large" />;
+    } else {
+      content = (
+        <View>
+          <Text style={{ paddingBottom: 40 }}>
+            <Text>you are</Text>
+            <Text style={{ fontWeight: "bold" }}> red-lobster9</Text>
+          </Text>
+          <Text
+            onPress={() => this.props.navigation.navigate("Game")}
+            style={{
+              fontSize: 40,
+              textAlign: "center",
+              textDecorationLine: "underline",
+              paddingBottom: 30
+            }}
+          >
+            play
+          </Text>
+          <Text
+            onPress={() => this.props.navigation.navigate("Game")}
+            style={{
+              fontSize: 12,
+              textAlign: "center",
+              textDecorationLine: "underline",
+              paddingBottom: 10
+            }}
+          >
+            play with friends
+          </Text>
+          <Text
+            onPress={() => this.props.navigation.navigate("Game")}
+            style={{
+              fontSize: 12,
+              textAlign: "center",
+              textDecorationLine: "underline",
+              paddingBottom: 20
+            }}
+          >
+            solo practice
+          </Text>
+        </View>
+      );
+    }
+
+    return <View style={Styles.mainContainer}>{content}</View>;
   }
 }

1db48474742e215d176fe24bf11cb223a6644781
Ofor Chinedu <chinedu@nomadinteractive.co>
2018-12-31 14:03:07 -0500

    add device id for android

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import Styles from "./style";
 import { View, Text, Button } from "react-native";
+import DeviceInfo from 'react-native-device-info';
 
 export default class HomeScreen extends React.Component {
+  state={
+    deviceId: ''
+  }
+  componentDidMount() {
+    this.getDeviceID()
+  }
+  
+  /**
+   * getDeviceID
+   *
+   * Gets device id
+   */
+  getDeviceID = () => {
+    const deviceId = DeviceInfo.getDeviceId();
+    alert(deviceId)
+  }
+  
   static navigationOptions = {
     header: null
   };
